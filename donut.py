a42ab15fecd0b3bfab2d15eb8762e82d868cdbae
Jay Dwayne <45174746+JayDwayne@users.noreply.github.com>
2024-04-08 20:50:17 +0300

    Defined height, width and range dimensions

--- donut.py
+++ donut.py
@@ -1,2 +1,31 @@
 import os
 from math import sin, cos
+
+def main():
+    # Initial values for angles
+    a = 0
+    b = 0
+
+    # Screen dimensions
+    height = 24
+    width = 80
+    
+    # Clear console command based on the operating system
+    clear = "cls" if os.name == "nt" else "clear"
+    
+    os.system(clear)
+    
+    while True:
+        # Initialize z-buffer and screen buffer
+        z = [0 for _ in range(4 * height * width)]
+        screen = [' ' for _ in range(height * width)]
+
+        # Loop for vertical angle
+        j = 0
+        while j < 6.28:
+            j += 0.07
+            # Loop for horizontal angle
+            i = 0
+            while i < 6.28:
+                i += 0.02
+                
